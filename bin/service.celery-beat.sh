9f98bb07409101d4146e05eea8b51e01982a08ca
Boris Rybalkin <ribalkin@gmail.com>
2024-08-30 17:35:35 +0100

    beat

--- bin/service.celery-beat.sh
+++ bin/service.celery-beat.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export HOME=$DIR/paperless/usr/src/paperless
 cd $HOME/src
 export PAPERLESS_CONFIGURATION_PATH=$SNAP_DATA/config/paperless.conf
+export PATH=$DIR/paperless/bin:$PATH
 exec $DIR/paperless/bin/python ${DIR}/paperless/usr/local/bin/celery --app paperless beat --loglevel INFO
