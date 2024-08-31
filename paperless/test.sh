dbf7e8511e47713dd22f29ecac7d52691ebce89d
Boris Rybalkin <ribalkin@gmail.com>
2024-08-31 09:32:18 +0100

    no test

--- paperless/test.sh
+++ paperless/test.sh
@@ -4,5 +4,5 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/paperless
-$BUILD_DIR/bin/python --version
-$BUILD_DIR/bin/python ${BUILD_DIR}/usr/local/bin/celery --version
+#$BUILD_DIR/bin/python --version
+#$BUILD_DIR/bin/python ${BUILD_DIR}/usr/local/bin/celery --version
