57f445c66e7e25e23b8ccba223d150783677cedd
Boris Rybalkin <ribalkin@gmail.com>
2024-09-01 19:06:00 +0100

    ca bundle

--- bin/service.gunicorn.sh
+++ bin/service.gunicorn.sh
@@ -5,4 +5,5 @@ export HOME=$DIR/paperless/usr/src/paperless
 cd $HOME/src
 export PAPERLESS_CONFIGURATION_PATH=$SNAP_DATA/config/paperless.conf
 $DIR/bin/wait-for-configure.sh
+export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.crt
 exec $DIR/paperless/sbin/python ${DIR}/paperless/usr/local/bin/gunicorn -c $DIR/paperless/usr/src/paperless/gunicorn.conf.py paperless.asgi:application
