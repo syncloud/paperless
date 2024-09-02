80c6a358faaab920095de6824dac1baaae8f3246
Boris Rybalkin <ribalkin@gmail.com>
2024-09-02 18:19:35 +0100

    fix static files

--- paperless/build.sh
+++ paperless/build.sh
@@ -9,4 +9,11 @@ cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 sed -i 's#bind.*=.*#bind="unix:/var/snap/paperless/common/web.socket"#g' ${BUILD_DIR}/usr/src/paperless/gunicorn.conf.py
 grep bind ${BUILD_DIR}/usr/src/paperless/gunicorn.conf.py
+
+cp --remove-destination -R ${BUILD_DIR}/usr/src/paperless/src/documents/static ${BUILD_DIR}/usr/src/paperless/static
+ls -la ${BUILD_DIR}/usr/src/paperless/static
+
+cp --remove-destination -R ${BUILD_DIR}/usr/local/lib/python3.11/site-packages/django/contrib/admin/static ${BUILD_DIR}/usr/src/paperless/static
+ls -la ${BUILD_DIR}/usr/src/paperless/static/admin
+
 cp --remove-destination -R ${DIR}/bin ${BUILD_DIR}/sbin
