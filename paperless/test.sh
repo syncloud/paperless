c9e71bde3ae19520b4f9ec0918586fa7d41d1b6f
Boris Rybalkin <ribalkin@gmail.com>
2024-09-02 09:58:29 +0100

    fix convert

--- paperless/test.sh
+++ paperless/test.sh
@@ -8,3 +8,4 @@ BUILD_DIR=${DIR}/../build/snap/paperless
 #$BUILD_DIR/bin/python ${BUILD_DIR}/usr/local/bin/celery --version
 $BUILD_DIR/sbin/tesseract --list-langs | grep eng
 $BUILD_DIR/sbin/tesseract --list-langs
+$BUILD_DIR/sbin/convert --version
