2f58a75895d94426e40db55d2eee5033667c057f
Boris Rybalkin <ribalkin@gmail.com>
2024-09-02 09:37:30 +0100

    more exports

--- bin/service.gunicorn.sh
+++ bin/service.gunicorn.sh
@@ -8,4 +8,6 @@ $DIR/bin/wait-for-configure.sh
 if [[ -f /var/snap/platform/current/CI_TEST ]]; then
   export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.crt
 fi
+export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.crt
+export CURL_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 exec $DIR/paperless/sbin/python ${DIR}/paperless/usr/local/bin/gunicorn -c $DIR/paperless/usr/src/paperless/gunicorn.conf.py paperless.asgi:application
