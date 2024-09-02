b0787e6b202d2499dd97cd621499d39387e2a247
Boris Rybalkin <ribalkin@gmail.com>
2024-09-02 11:04:09 +0100

    fix ca

--- bin/service.gunicorn.sh
+++ bin/service.gunicorn.sh
@@ -6,8 +6,8 @@ cd $HOME/src
 export PAPERLESS_CONFIGURATION_PATH=$SNAP_DATA/config/paperless.conf
 $DIR/bin/wait-for-configure.sh
 if [[ -f /var/snap/platform/current/CI_TEST ]]; then
-  export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.crt
+  export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 fi
-export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.crt
+export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 export CURL_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 exec $DIR/paperless/sbin/python ${DIR}/paperless/usr/local/bin/gunicorn -c $DIR/paperless/usr/src/paperless/gunicorn.conf.py paperless.asgi:application
