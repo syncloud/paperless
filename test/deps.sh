b5197c41e816683cc4c7a7a4347f3c1117cffd0e
Boris Rybalkin <ribalkin@gmail.com>
2026-01-10 18:02:30 +0000

    lib

--- test/deps.sh
+++ test/deps.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
-DIR=$( cd "$( dirname "$0" )" && pwd )
 
-while ! apt-get update; do
-  sleep 1
-  echo "retry"
-done
-apt-get install -y sshpass openssh-client wget
+apt-get update
+apt-get install -y sshpass openssh-client
 pip install -r requirements.txt
