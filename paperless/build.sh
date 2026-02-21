d7a4082ab7d431472081a2f78f8f9b73b2d40744
Boris Rybalkin <ribalkin@gmail.com>
2026-02-21 11:22:02 +0000

    samples

--- paperless/build.sh
+++ paperless/build.sh
@@ -42,7 +42,7 @@ cp paperless-ngx-dev/src/paperless/settings.py ${BUILD_DIR}/usr/src/paperless/sr
 #sed -i 's#username=data.get("preferred_username"),#username=data.get("preferred_username"), groups=data.get("groups"),#g' ${BUILD_DIR}/usr/local/lib/python3.11/site-packages/allauth/socialaccount/providers/openid_connect/provider.py
 #grep groups ${BUILD_DIR}/usr/local/lib/python3.11/site-packages/allauth/socialaccount/providers/openid_connect/provider.py
 
-cp -r paperless-ngx-dev/src/documents/tests/samples/* ${DIR}/../build/samples
+cp -r paperless-ngx-dev/src/documents/tests/samples ${DIR}/../build
 
 SNAP=/snap/paperless/current
 mkdir -p $SNAP
