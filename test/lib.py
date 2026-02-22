15922fa18943b85bd533a7f39a8a3b0b283dfb87
Boris Rybalkin <ribalkin@gmail.com>
2026-02-22 18:28:12 +0000

    add upgrade test with pre/post data validation
    Install from store, upload document, upgrade to new build, verify
    document persists, upload again to confirm post-upgrade functionality.

---
+++ test/lib.py
@@ -0,0 +1,45 @@
+from os.path import dirname, join
+from selenium.webdriver.common.by import By
+
+DIR = dirname(__file__)
+
+
+def login(selenium, device_user, device_password):
+    selenium.open_app()
+    selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
+    password = selenium.find_by(By.ID, "password-textfield")
+    password.send_keys(device_password)
+    selenium.screenshot('login')
+    selenium.find_by(By.ID, "sign-in-button").click()
+    selenium.find_by(By.ID, "accept-button").click()
+    selenium.find_by(By.XPATH, "//button[contains(.,'Sign up')]").click()
+    selenium.find_by(By.XPATH, "//h4[contains(.,'Paperless-ngx is running!')]")
+    selenium.screenshot('main')
+
+
+def login_existing_user(selenium, device_user, device_password):
+    selenium.open_app()
+    selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
+    password = selenium.find_by(By.ID, "password-textfield")
+    password.send_keys(device_password)
+    selenium.screenshot('login-post')
+    selenium.find_by(By.ID, "sign-in-button").click()
+    selenium.find_by(By.ID, "accept-button").click()
+    selenium.find_by(By.XPATH, "//h4[contains(.,'Paperless-ngx is running!')]")
+    selenium.screenshot('main-post')
+
+
+def upload_document(selenium, mode):
+    file = selenium.find_by(By.XPATH, "//input[@type='file']")
+    selenium.driver.execute_script("arguments[0].removeAttribute('class')", file)
+    file.clear()
+    file.send_keys(join(DIR, '..', 'build', 'samples', 'simple.pdf'))
+    selenium.invisible_by(By.XPATH, "//p[contains(.,'Upload complete, waiting...')]")
+    selenium.find_by(By.XPATH, "//span[contains(.,'Dismiss completed')]")
+    selenium.screenshot('uploaded-{0}'.format(mode))
+
+
+def check_document_list(selenium, mode):
+    selenium.find_by(By.XPATH, "//a[contains(@href, '/documents')]").click()
+    selenium.find_by(By.XPATH, "//pngx-document-card")
+    selenium.screenshot('documents-{0}'.format(mode))
--- test/upgrade.py
+++ test/upgrade.py
@@ -4,6 +4,7 @@ from syncloudlib.integration.hosts import add_host_alias
 from syncloudlib.integration.installer import local_install
 from syncloudlib.http import wait_for_rest
 import requests
+from test import lib
 
 TMP_DIR = '/tmp/syncloud'
 
@@ -11,7 +12,7 @@ TMP_DIR = '/tmp/syncloud'
 @pytest.fixture(scope="session")
 def module_setup(request, device, artifact_dir):
     def module_teardown():
-        device.run_ssh('journalctl > {0}/refresh.journalctl.log'.format(TMP_DIR), throw=False)
+        device.run_ssh('journalctl > {0}/upgrade.journalctl.log'.format(TMP_DIR), throw=False)
         device.scp_from_device('{0}/*'.format(TMP_DIR), artifact_dir)
         run('cp /videos/* {0}'.format(artifact_dir), shell=True)
         check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
@@ -26,10 +27,32 @@ def test_start(module_setup, app, device_host, domain, device):
     device.run_ssh('mkdir {0}'.format(TMP_DIR), throw=False)
 
 
-def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain, app_dir):
+def test_install_from_store(device, app_domain):
     device.run_ssh('snap remove paperless')
     device.run_ssh('snap install paperless', retries=10)
+    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 100)
+
+
+def test_login_pre_upgrade(selenium, device_user, device_password):
+    lib.login(selenium, device_user, device_password)
+
+
+def test_upload_pre_upgrade(selenium):
+    lib.upload_document(selenium, 'pre')
+
+
+def test_upgrade(device_host, device_password, app_archive_path, app_domain):
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 100)
 
 
+def test_login_post_upgrade(selenium, device_user, device_password):
+    lib.login_existing_user(selenium, device_user, device_password)
+
+
+def test_check_pre_upgrade_data(selenium):
+    lib.check_document_list(selenium, 'post-check')
+
+
+def test_upload_post_upgrade(selenium):
+    lib.upload_document(selenium, 'post')
