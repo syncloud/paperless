217220d03f514c663db7ea42e232d711e53c0ba4
Boris Rybalkin <ribalkin@gmail.com>
2026-03-28 14:04:48 +0000

    Grant default document permissions to non-superuser OIDC users on signup

--- paperless/adapter.py
+++ paperless/adapter.py
@@ -1,11 +1,17 @@
+import logging
 from urllib.parse import quote
 
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.core import context
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
+from django.contrib.auth.models import Group, Permission, User
 from django.forms import ValidationError
 from django.urls import reverse
+
+from documents.models import Document
+
+logger = logging.getLogger("paperless.auth")
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
@@ -14,8 +20,13 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         Check whether the site is open for signups, which can be
         disabled via the ACCOUNT_ALLOW_SIGNUPS setting.
         """
+        if (
+            User.objects.exclude(username__in=["consumer", "AnonymousUser"]).count()
+            == 0
+            and Document.global_objects.count() == 0
+        ):
+            return True
         allow_signups = super().is_open_for_signup(request)
-        # Override with setting, otherwise default to super.
         return getattr(settings, "ACCOUNT_ALLOW_SIGNUPS", allow_signups)
 
     def pre_authenticate(self, request, **credentials):
@@ -61,6 +72,25 @@ class CustomAccountAdapter(DefaultAccountAdapter):
             path = path.replace("UID-KEY", quote(key))
             return settings.PAPERLESS_URL + path
 
+    def save_user(self, request, user, form, commit=True):  # noqa: FBT002
+        if (
+            User.objects.exclude(username__in=["consumer", "AnonymousUser"]).count()
+            == 0
+            and Document.global_objects.count() == 0
+        ):
+            logger.debug(f"Creating initial superuser `{user}`")
+            user.is_superuser = True
+            user.is_staff = True
+
+        user = super().save_user(request, user, form, commit)
+        group_names = getattr(settings, "ACCOUNT_DEFAULT_GROUPS", [])
+        if len(group_names) > 0:
+            groups = Group.objects.filter(name__in=group_names)
+            logger.debug(f"Adding default groups to user `{user}`: {group_names}")
+            user.groups.add(*groups)
+            user.save()
+        return user
+
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request, sociallogin):
@@ -69,7 +99,6 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         disabled via the SOCIALACCOUNT_ALLOW_SIGNUPS setting.
         """
         allow_signups = super().is_open_for_signup(request, sociallogin)
-        # Override with setting, otherwise default to super.
         return getattr(settings, "SOCIALACCOUNT_ALLOW_SIGNUPS", allow_signups)
 
     def get_connect_redirect_url(self, request, socialaccount):
@@ -81,10 +110,6 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         return url
 
     def populate_user(self, request, sociallogin, data):
-        """
-        Populate the user with data from the social account. Stub is kept in case
-        global default permissions are implemented in the future.
-        """
         user = super().populate_user(request, sociallogin, data)
         extra_data = sociallogin.account.extra_data
         # In allauth >= 65.11, OIDC extra_data is nested under 'userinfo' or 'id_token'
@@ -94,3 +119,48 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
             if settings.SOCIALACCOUNT_ADMIN_GROUP in groups:
                 user.is_superuser = True
         return user
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        group_names = getattr(settings, "SOCIAL_ACCOUNT_DEFAULT_GROUPS", [])
+        if len(group_names) > 0:
+            groups = Group.objects.filter(name__in=group_names)
+            logger.debug(
+                f"Adding default social groups to user `{user}`: {group_names}",
+            )
+            user.groups.add(*groups)
+            user.save()
+        if not user.is_superuser:
+            _assign_default_permissions(user)
+        return user
+
+
+def _assign_default_permissions(user):
+    """
+    Grant minimum document permissions to non-superuser OIDC users so they
+    can view and manage documents without admin intervention.
+    """
+    default_perms = Permission.objects.filter(
+        content_type__app_label="documents",
+        codename__in=[
+            "view_document",
+            "add_document",
+            "change_document",
+            "delete_document",
+            "view_tag",
+            "add_tag",
+            "view_correspondent",
+            "add_correspondent",
+            "view_documenttype",
+            "add_documenttype",
+            "view_savedview",
+            "add_savedview",
+            "change_savedview",
+            "delete_savedview",
+            "view_uisettings",
+            "add_uisettings",
+            "change_uisettings",
+        ],
+    )
+    user.user_permissions.add(*default_perms)
+    logger.debug(f"Assigned default document permissions to user `{user}`")
